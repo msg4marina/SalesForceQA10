fb50175bba98d17904b6f758aec745774a53fdcf
Marina Maslova <msg4marina@gmail.com>
2021-05-24 21:04:49 +0300

    UpDate

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -14,8 +14,10 @@ public class LoginTest extends BaseTest {
                 .isPageOpened();
         assertTrue(isOpened, "Страница логина не открылась");
         isOpened = loginPage
-                .login("dmitryrak11-fcsk@force.com", "password01")
+                .login("legal.verdegrano-nhse@force.com", "2Password")
                 .isPageOpened();
         assertTrue(isOpened, "Home Page не открылась");
     }
+
+
 }
