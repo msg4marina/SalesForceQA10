5807b117f8a9efe0f7fc32f4f96e8a957a7ce9a8
Marina Maslova <msg4marina@gmail.com>
2021-05-29 13:53:37 +0300

    @test заменен на: @test (retryAnalyzer = Retry.class)

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -1,13 +1,11 @@
 package tests;
-
 import org.openqa.selenium.By;
 import org.testng.annotations.Test;
-
 import static org.testng.Assert.assertTrue;
 
 public class LoginTest extends BaseTest {
 
-    @Test
+    @Test (retryAnalyzer = Retry.class)
     public void login() {
         boolean isOpened = loginPage
                 .open()
