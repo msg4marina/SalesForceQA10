4ce7c218f1ec9c9be4b376122b475f3f8f087642
Marina Maslova <msg4marina@gmail.com>
2021-06-07 08:36:56 +0300

    Up2Date+allure added +initial commit ContactsTest

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -4,14 +4,19 @@ import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.testng.ITestContext;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Listeners;
+import org.testng.annotations.Optional;
 import pages.AccountListPage;
 import pages.ContactsListPage;
 import pages.LoginPage;
 import tests.base.TestListener;
 
+import java.nio.charset.MalformedInputException;
 import java.util.concurrent.TimeUnit;
 
 @Listeners(TestListener.class)
@@ -22,15 +27,22 @@ public abstract class BaseTest {
     AccountListPage accountListPage;
     ContactsListPage contactsListPage;
 
-    @BeforeMethod
-    public void setUp() {
-        WebDriverManager.chromedriver().setup();
-        ChromeOptions options = new ChromeOptions();
-        JavascriptExecutor jse = (JavascriptExecutor)driver;
-        options.addArguments("--start-maximized");
-        options.addArguments("--disable-notifications");
+    @BeforeMethod(description = "Open browser")
+    public void setUp(@Optional("chrome") String browser, ITestContext testContext) throws MalformedInputException {
+        System.out.println(browser);
+        if (browser.equals("chrome")) {
+            WebDriverManager.chromedriver().setup();
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--start-maximized");
+            driver = new ChromeDriver(options);
 
-        driver = new ChromeDriver(options);
+        } else if (browser.equals("edge")) {
+            WebDriverManager.edgedriver().setup();
+            EdgeOptions options = new EdgeOptions();
+            driver = new EdgeDriver(options);
+        }
+        testContext.setAttribute("driver", driver);
+
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.manage().window().maximize();
         loginPage = new LoginPage(driver);
@@ -38,6 +50,7 @@ public abstract class BaseTest {
         contactsListPage = new ContactsListPage(driver);
     }
     @AfterMethod(alwaysRun = true)
-    public void tearDown() { //driver.quit();
+    public void tearDown() {
+        driver.quit();
     }
 }
